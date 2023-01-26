c8e7363dea06c2d4492c2a30e78096889dcb4ac9
Lapo Luchini <lapo@lapo.it>
2023-01-26 12:55:00 +0100

    Update some patches.

--- files/patch-src_java_org_apache_cassandra_db_commitlog_MemoryMappedSegment.java
+++ files/patch-src_java_org_apache_cassandra_db_commitlog_MemoryMappedSegment.java
@@ -1,11 +1,11 @@
---- src/java/org/apache/cassandra/db/commitlog/MemoryMappedSegment.java.orig	2019-10-24 16:42:10 UTC
+--- src/java/org/apache/cassandra/db/commitlog/MemoryMappedSegment.java.orig	2022-12-07 20:55:02 UTC
 +++ src/java/org/apache/cassandra/db/commitlog/MemoryMappedSegment.java
 @@ -90,7 +90,7 @@ public class MemoryMappedSegment extends CommitLogSegm
          {
              throw new FSWriteError(e, getPath());
          }
--        NativeLibrary.trySkipCache(fd, startMarker, nextMarker, logFile.getAbsolutePath());
-+//        NativeLibrary.trySkipCache(fd, startMarker, nextMarker, logFile.getAbsolutePath());
+-        NativeLibrary.trySkipCache(fd, startMarker, nextMarker, logFile.absolutePath());
++//        NativeLibrary.trySkipCache(fd, startMarker, nextMarker, logFile.absolutePath());
      }
  
      @Override
