70ea72bb9c15c41d6d8eaafa56bfd9c09ff96783
Lapo Luchini <lapo@lapo.it>
2023-01-28 00:02:19 +0100

    Update remaining patches.

--- files/patch-src_java_org_apache_cassandra_utils_NativeLibrary.java
+++ files/patch-src_java_org_apache_cassandra_utils_NativeLibrary.java
@@ -1,10 +1,10 @@
---- src/java/org/apache/cassandra/utils/NativeLibrary.java.orig	2020-08-28 13:55:56 UTC
+--- src/java/org/apache/cassandra/utils/NativeLibrary.java.orig	2022-12-07 20:55:03 UTC
 +++ src/java/org/apache/cassandra/utils/NativeLibrary.java
-@@ -143,7 +143,10 @@ public final class NativeLibrary
-         else if (osName.contains("windows"))
-             return WINDOWS;
+@@ -140,7 +140,10 @@ public final class NativeLibrary
+         else if (osName.contains("mac"))
+             return MAC;
  
--        logger.warn("the current operating system, {}, is unsupported by cassandra", osName);
+-        logger.warn("the current operating system, {}, is unsupported by Cassandra", osName);
 +        if (osName.contains("bsd"))
 +            logger.warn("the current operating system, {}, is fantastic, though not officially supported by cassandra", osName);
 +        else
