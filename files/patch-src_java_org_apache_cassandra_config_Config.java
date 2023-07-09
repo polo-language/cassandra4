dfe7fb1b3a1495e4f5114ca654a3e5ee7dda60de
Angelo Polo <language.devel@gmail.com>
2023-07-09 12:03:21 -0400

    Add more patches

--- files/patch-src_java_org_apache_cassandra_config_Config.java
+++ files/patch-src_java_org_apache_cassandra_config_Config.java
@@ -1,9 +1,9 @@
---- src/java/org/apache/cassandra/config/Config.java.orig	2023-07-09 16:40:06 UTC
+--- src/java/org/apache/cassandra/config/Config.java.orig	2023-05-25 14:13:38 UTC
 +++ src/java/org/apache/cassandra/config/Config.java
 @@ -437,7 +437,7 @@ public class Config
      @Replaces(oldName = "key_cache_save_period", converter = Converters.SECONDS_CUSTOM_DURATION)
      public volatile DurationSpec.IntSecondsBound key_cache_save_period = new DurationSpec.IntSecondsBound("4h");
-
+ 
 -    public String row_cache_class_name = "org.apache.cassandra.cache.OHCProvider";
 +    public String row_cache_class_name = "org.apache.cassandra.cache.SerializingCacheProvider";
      @Replaces(oldName = "row_cache_size_in_mb", converter = Converters.MEBIBYTES_DATA_STORAGE_LONG, deprecated = true)
